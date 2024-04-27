3109d7df9fb70b086c438198eb5dc4ff9acbd9d5
victorpreston <prestonvictor25@gmail.com>
2024-04-27 07:52:04 +0300

    + footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer({ onClear = () => { }, children }) {
     return (
         <footer className="py-2 px-3 sm:px-6 flex items-center gap-2 justify-between">
             <div className="flex items-center gap-[6px]">
-                <Button size="icon" asChild variant="secondary"><a target="_blank" href="https://github.com/r2hu1/ide"><Star className="h-4 w-4" /></a></Button>
-                <Button size="icon" asChild variant="secondary"><a target="_blank" href="https://github.com/r2hu1/ide/fork"><GitFork className="h-4 w-4" /></a></Button>
+                <Button size="icon" asChild variant="secondary"><a target="_blank" href=""><Star className="h-4 w-4" /></a></Button>
+                <Button size="icon" asChild variant="secondary"><a target="_blank" href=""><GitFork className="h-4 w-4" /></a></Button>
             </div>
             <div className="flex items-center gap-[6px]">
                 <Button size="sm" onClick={onClear}>Clear</Button>

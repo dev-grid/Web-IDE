e3d715a6fe33a70e1f8605202cc22c9c0706cc6b
victorpreston <prestonvictor25@gmail.com>
2024-04-27 08:04:12 +0300

    ++ sample code

--- hooks/useHtml.jsx
+++ hooks/useHtml.jsx
@@ -2,7 +2,7 @@ const { useState } = require("react");
 
 const useHtml = () => {
     const [html, setHtml] = useState(`<button onClick="handleClick()">
-  Click Me
+  Start Here
 </button>`);
 
     const setHtmlValue = (value) => {
